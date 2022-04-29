aafcc04622568a979b926a0472d5084626622370
htam <hdfair@gmail.com>
2022-04-29 16:25:37 -0400

    added all icons to service worker js

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,13 @@ const FILES_TO_CACHE = [
   "./js/index.js",
   "./js/idb.js",
   "./icons/icon-72x72.png",
-  // add rest of icons her
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
 ];
 
 // Respond with cached resources
