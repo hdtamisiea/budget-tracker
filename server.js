379cff0c44ebe94194c6e4204205668b6478c276
htam <hdfair@gmail.com>
2022-05-03 13:17:01 -0400

    Deploying

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
-  // useUnifiedTopology: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  // useFindAndModify: false
 });
 
 // routes
